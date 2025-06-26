abde74a808ba42d83b74fd4f96323c29d7789cbf
aldoroman27 <juggeraldo@gmail.com>
2025-06-26 14:55:42 -0600

    Octavo commit de la semana, más modificaciones y configutaciones

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_cors import CORS
 from routes.tickets import ticket_bp
 from routes.login import auth_bp
-
 app = Flask(__name__)
 CORS(app)
 
@@ -10,7 +9,9 @@ CORS(app)
 def index():
     return 'Workin´good'
 
+#Todas nuestras funciones que tenemos disponibles dentro de nuestro backend de cada componente.
 app.register_blueprint(ticket_bp)
 app.register_blueprint(auth_bp)
+
 if __name__ == '__main__':
     app.run(debug=True)
