057c3aee9be9244ce1c685d3e5aae619d59ca427
aldoroman27 <juggeraldo@gmail.com>
2025-07-23 14:55:40 -0600

    Hola

--- pages/AdminPages/ConsultarTicket.jsx
+++ pages/AdminPages/ConsultarTicket.jsx
@@ -26,7 +26,7 @@ export const ConsultarTicket = () => {
           }
         });
         //Filtramos si el ticket está en pendiente
-        const pendientes = response.data.filter(ticket => ticket.estado.toLoweCase()=== 'pendiente');
+        const pendientes = response.data.filter(ticket => ticket.estado.toLowerCase()=== 'pendiente');
         //Seteamos el ticket y lo pasamos como parametro
         setTickets(pendientes);
       } catch (error) {
