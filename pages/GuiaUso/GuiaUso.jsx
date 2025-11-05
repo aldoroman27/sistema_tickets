db5782f86dd0c0bf8b10dbb5031d2dfca1bc217e
aldoroman27 <juggeraldo@gmail.com>
2025-11-05 09:52:33 -0600

    Cambiamos el nombre del pdf, para actualizar el contenido de la guía de uso

--- pages/GuiaUso/GuiaUso.jsx
+++ pages/GuiaUso/GuiaUso.jsx
@@ -4,7 +4,7 @@ import './GuiaUso.css';
 export const GuiaUso = () => {
   const handleDescargar = () => {
     const link = document.createElement('a');
-    link.href = '/Guia_de_uso_DEMO.pdf'; //El pdf se encuentra en la carpeta PUBLIC
+    link.href = '/Guia_de_uso_v0.0.14.pdf'; //El pdf se encuentra en la carpeta PUBLIC
     link.download = 'Guia_de_uso_STbyAGRDM.pdf';//Se descargará con este nombre
     link.click();
   };
